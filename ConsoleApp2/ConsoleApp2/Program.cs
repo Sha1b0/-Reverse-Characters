01bae7c4e0740be27d9a47f0c870cd69345abe8a
Sha1b0 ChAkRaB0rTy <68947461+Sha1b0@users.noreply.github.com>
2022-12-14 19:32:53 +0600

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace xyz
+{
+    class xyz
+    {
+
+        static void Main(string[] args)
+        {
+            string firstname;
+            string lastname;
+            string city;
+            
+            
+            
+            Console.Write("Enter your First Name:");
+            firstname =Console.ReadLine();
+
+
+            Console.Write("Enter your Last Name:");
+            lastname = Console.ReadLine();
+
+
+            Console.Write("Enter your City:");
+            city = Console.ReadLine();
+
+
+            char[] FirstnameArray = firstname.ToCharArray();
+            Array.Reverse(FirstnameArray);
+
+            char[] LastnameArray = lastname.ToCharArray();
+            Array.Reverse(LastnameArray);
+
+            char[] CityArray = city.ToCharArray();
+            Array.Reverse(CityArray);
+
+            string result = "";
+
+
+            foreach (char item in FirstnameArray)
+            {
+                 result += item;
+            }
+
+            result += " ";
+
+
+
+            foreach (char item in LastnameArray)
+            {
+                result += item;
+            }
+            result += " ";
+
+            foreach (char item in CityArray)
+            {
+                result += item;
+            }
+
+            Console.WriteLine("Result:" + result);
+            Console.Beep();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
